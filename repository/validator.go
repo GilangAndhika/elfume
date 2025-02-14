5624db05173e21510ccc95313c64fade5026b115
GilangAndhika <gryzlegrizz@gmail.com>
2025-02-14 20:29:19 +0700

    Add password hashing and comparison functions

---
+++ repository/validator.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal("Failed to hash password", err)
+	}
+	return string(hashedPassword)
+}
+
+func ComparePassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
