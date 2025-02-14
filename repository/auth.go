c0dabc1b1ab2dca1260a1ef83470eb69cae79525
GilangAndhika <gryzlegrizz@gmail.com>
2025-02-14 20:30:01 +0700

    Add function to create account

--- repository/auth.go
+++ repository/auth.go
@@ -3,4 +3,46 @@ package repository
 import (
 	"elfume/config"
 	"elfume/model"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+func CreateAccount(ctx *fiber.Ctx) error {
+	// Get connection
+	client := config.GetDB()
+	collection := client.Database("elfume").Collection("users")
+
+	// Parse request body into user struct
+	var user model.User
+	if err := ctx.BodyParser(&user); err != nil {
+		return ctx.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+		})
+	}
+
+	// Hash password
+	user.Password = HashPassword(user.Password)
+
+	// Insert user data
+	_, err := collection.InsertOne(ctx.Context(), bson.M{
+		"username":   user.Username,
+		"email":      user.Email,
+		"password":   user.Password,
+		"phone":      user.Phone,
+		"role_id":    model.RoleCustomer,
+		"created_at": primitive.NewDateTimeFromTime(time.Now()),
+		"updated_at": primitive.NewDateTimeFromTime(time.Now()),
+	})
+	if err != nil {
+		return ctx.Status(500).JSON(fiber.Map{
+			"message": "Failed to create account",
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Account created successfully",
+	})
+}
