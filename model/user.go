cd5c6d183b5286e296971c64dd563a12491e68e0
GilangAndhika <gryzlegrizz@gmail.com>
2025-02-15 08:47:18 +0700

    Refactor user and role models to use primitive.ObjectID for IDs

--- model/user.go
+++ model/user.go
@@ -1,21 +1,23 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID        primitive.ObjectID `json:"user_id" bson:"_id"`
+	UserID        primitive.ObjectID `json:"user_id" bson:"_id"`
 	Username  string             `json:"username" bson:"username"`
 	Email     string             `json:"email" bson:"email"`
 	Password  string             `json:"password" bson:"password"`
 	Phone     string             `json:"phone" bson:"phone"`
-	RoleID    string             `json:"role_id" bson:"role_id"`
+	RoleID    primitive.ObjectID `json:"role_id" bson:"role_id"`
 	CreatedAt primitive.DateTime `json:"created_at" bson:"created_at"`
 	UpdatedAt primitive.DateTime `json:"updated_at" bson:"updated_at"`
 }
 
 type Role struct {
-	ID       string `json:"role_id" bson:"_id"`
-	RoleName string `json:"role_name" bson:"role_name"`
+	RoleID       primitive.ObjectID `json:"role_id" bson:"_id"`
+	RoleName string             `json:"role_name" bson:"role_name"`
 }
 
 const (
